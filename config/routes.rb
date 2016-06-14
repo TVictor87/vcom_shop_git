3e627a7d671289276552cb4b4988542a5b0ef434
Romaboy <romadzao@gmail.com>
2016-06-14 18:04:08 +0300

    seeds

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,26 @@ Rails.application.routes.draw do
 
   resources :pages, olny: [:index, :show]
 
-  get 'for_women', to: 'pages#categories', as: 'for_women', defaults: { url: 'for_women', column: 'url_ru' }
-  get 'for_women/:category_url', to: 'pages#catalog', defaults: { url: 'for_women', column: 'url_ru' }
+  get 'zhenshchinam', to: 'pages#categories', as: 'women_ru', defaults: { url: 'zhenshchinam', column: 'url_ru' }
+  get 'zhenshchinam/:category_url', to: 'pages#catalog', defaults: { url: 'zhenshchinam', column: 'url_ru' }
+  get 'women', to: 'pages#categories', as: 'women_en', defaults: { url: 'women', column: 'url_en' }
+  get 'women/:category_url', to: 'pages#catalog', defaults: { url: 'women', column: 'url_en' }
+  get 'zhinkam', to: 'pages#categories', as: 'women_uk', defaults: { url: 'zhinkam', column: 'url_en' }
+  get 'zhinkam/:category_url', to: 'pages#catalog', defaults: { url: 'zhinkam', column: 'url_en' }
+
+  get 'muzhchinam', to: 'pages#categories', as: 'men_ru', defaults: { url: 'muzhchinam', column: 'url_ru' }
+  get 'muzhchinam/:category_url', to: 'pages#catalog', defaults: { url: 'muzhchinam', column: 'url_ru' }
+  get 'men', to: 'pages#categories', as: 'men_en', defaults: { url: 'men', column: 'url_ru' }
+  get 'men/:category_url', to: 'pages#catalog', defaults: { url: 'men', column: 'url_ru' }
+  get 'cholovikam', to: 'pages#categories', as: 'men_uk', defaults: { url: 'cholovikam', column: 'url_ru' }
+  get 'cholovikam/:category_url', to: 'pages#catalog', defaults: { url: 'cholovikam', column: 'url_ru' }
+
+  get 'detyam', to: 'pages#categories', as: 'child_ru', defaults: { url: 'detyam', column: 'url_ru' }
+  get 'detyam/:category_url', to: 'pages#catalog', defaults: { url: 'detyam', column: 'url_ru' }
+  get 'child', to: 'pages#categories', as: 'child_en', defaults: { url: 'child', column: 'url_ru' }
+  get 'child/:category_url', to: 'pages#catalog', defaults: { url: 'child', column: 'url_ru' }
+  get 'dityam', to: 'pages#categories', as: 'child_uk', defaults: { url: 'dityam', column: 'url_ru' }
+  get 'dityam/:category_url', to: 'pages#catalog', defaults: { url: 'dityam', column: 'url_ru' }
 
   # Dynamic Pages route
   get '*urls' => 'pages#show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,42 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+Site.delete_all
+Category.delete_all
+
 # Create admin user
 User.create(email: 'admin@gmail.com', password: '1q2w3e4r', first_name: 'Admin', last_name: 'Admin', role: 'admin')
 
 # Create base sites
 Site.create(title_ru: 'Vkolgotkah.com', title_uk: 'Vkolgotkah.com', title_en: 'Vkolgotkah.com', constant_name: 'Vkolgotkah')
 Site.create(title_ru: 'Панчохи.укр', title_uk: 'Панчохи.укр', title_en: 'Панчохи.укр', constant_name: 'Panchohy')
+
+
+Category.create(
+	url_ru: 'zhenshchinam',
+	url_en: 'women',
+	url_uk: 'zhinkam',
+	name_ru: 'Для женщин',
+	name_en: 'For women',
+	name_uk: 'Для жінок'
+)
+Category.create(
+	url_ru: 'muzhchinam',
+	url_en: 'men',
+	url_uk: 'cholovikam',
+	name_ru: 'Для мужчин',
+	name_en: 'For men',
+	name_uk: 'Для чоловіків'
+)
+Category.create(
+	url_ru: 'detyam',
+	url_en: 'child',
+	url_uk: 'dityam',
+	name_ru: 'Для детей',
+	name_en: 'For child',
+	name_uk: 'Для дітей'
+)
 
 # TODO: Create base pages: Main page with description
 # TODO: Create base pages: For Mans, For womans, For Children
